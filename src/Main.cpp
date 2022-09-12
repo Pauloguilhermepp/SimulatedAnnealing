2e8526d09ac9184f4c54c33c65ba453664647384
Paulo Guilherme <pauloguilherme@usp.br>
2022-09-11 23:35:27 -0300

    Add new test with the Simulated Annealing class

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,8 +10,18 @@ long double evaluationFunc(std::vector<long double> vector){
     return sum;
 }
 
+long double evaluationFunc2(std::vector<long double> vector){
+    long double sum = 0;
+
+    for(int i = 0; i < vector.size(); i++){
+        sum += pow(vector[i] - 0.5, 2);
+    }
+
+    return sum;
+}
+
 int main(){
-    SimulatedAnnealing sa(3, 100, 50, 0.1, 0.1, 0, 1, &evaluationFunc);
+    SimulatedAnnealing sa(3, 100, 50, 0.9, 0.1, 0, 1, &evaluationFunc2);
     sa.run();
 
     return 0;
